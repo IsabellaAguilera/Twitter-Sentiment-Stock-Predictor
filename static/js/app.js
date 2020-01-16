82963ac91da17195bbf2b275b4228a19314aa78d
Isabella Aguilera <isabellaaguilera92@gmail.com>
2020-01-16 16:59:47 -0500

    update 3

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,74 @@
-var svgWidth = 960;
-var svgHeight = 500;
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
+function buildPredictionPlot(){
+
+    var url = "/company"
+
+    d3.json(url).then(function(response) {
+        var data = response;
+
+        var twitterPercentages = data.map(function(record) {
+            return record[''];
+        });
+
+        var stockPrices = data.map(function(record) {
+            return record[''];
+        });
+
+        var stockDays = data.map(function(record) {
+            return record[''];
+        });
+
+        var pieColors = ['rgb(255, 0, 0)','rgb(0, 0, 238)'];
+
+        var trace1 = {
+            values: twitterPercentages,
+            labels: ["% of Positive Tweets", "% of Negative Tweets"],
+            type: "pie",
+            marker: {
+                colors: pieColors
+            }
+        };
+
+        var trace2 = {
+            x: stockDays,
+            y: stockPrices,
+            type: "line",
+
+
+        };
+
+        var layout1 = {
+            showlegend: true,
+            title: "Twitter Positive/Negative Percentagas"
+
+        };
+
+        var layout2 = {
+            showlegend: true,
+            title: "Stock Closing Price Over 7 Days"
+        };
+
+        var data1 = [trace1];
+        var data2 = [trace2];
+
+        Plotly.plot("pie", data1, layout1);
+        Plotly.plot("line", data2, layout2);
+
+    });
+
 };
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+function buySell() {
 
-var svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    var value = data.map(function(record) {
+        return record[''];
+    });
 
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    if(value === 1){
+        $("div#buysell").append('Buy');
+    }
+    else{
+        $("div#buysell").append('Sell');
+    }
+
+};
