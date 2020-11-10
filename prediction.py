58a47617699f3dbc6479ad6a7e68e74a3e2fb29a
Isabella Aguilera <isabellaaguilera92@gmail.com>
2020-11-10 17:04:32 -0500

    prediciton.py update 3

--- prediction.py
+++ prediction.py
@@ -5,9 +5,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-import tensorflow
-from tensorflow import keras
-from keras.models import load_model
+import tensorflow as tf
+from tf.python.keras.models import load_model
 import json
 
 def xScaler():
