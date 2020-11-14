eedd30c0b888f32bb5d9ff6c9a441bd44adf5243
Isabella Aguilera <isabellaaguilera92@gmail.com>
2020-11-13 19:36:33 -0500

    files/folders cleanup 2

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# final-project
+# Twitter Sentiment Stock Predictor
+
+## Overview
+Twitter Sentiment Stock Predictor is a full-stack app that allows the user to input a company name and view the percentage of positive and negative feedback on Twitter about that company's stock, the stock's closing prices over the past 5 days, as well as whether or not the user should buy or sell stock in that company. This app uses a Twitter Sentiment machine learning model, trained with Twitter sentiment data gathered from an API call to Twitter using the tweepy library. The model is trained to predict whether the user should buy or sell stock in the company. The app also retrieves the closing price data using the Yahoo Finance (yfinance) library. When the user inputs a company name, the app retrieves the company's data if the name is present in a csv file with a list of over 3000 top companies. Once the name can be verified from the list, the API call is made to Twitter to retrieve sentiment data for the Twitter Sentiment model, and closing price data is retrieved from the yfinance library. The user is then prompted to another page to view the model's analysis ("Buy" or "Sell"), as well as visualizations of the Twitter sentiment and closing price data for the company's stock. 
+
+![home_page](https://github.com/IsabellaAguilera/Twitter-Sentiment-Stock-Predictor/blob/master/images/home_page.png?raw=true)
+
+
+## Technologies/Frameworks
+Front End
+* HTML/CSS
+* JavaScript
+* BootStrap
+
+Back End
+* Python
+* Flask
+* JSON
+
+## Libraries
+* Plotly.js
+* TensorFlow
+* Sklearn
+* YFinance
+* Tweepy
+* NumPy
+* Pandas
+
+![visualizations_page](https://github.com/IsabellaAguilera/Twitter-Sentiment-Stock-Predictor/blob/master/images/visualizations_page.png?raw=true)
+
+## Future Improvements
+* Creating a more visually appealing and engaging UI in terms of the data visualizations
+* Using Twitter Sentiment data over a longer period of time (i.e. 1 month) to make more accurate "Buy or Sell" predictions with our ML model
+* Allowing users to search by company name or by the company's stock ticker
+
+## Access to Application
+* [Link](https://twitterstockapp.herokuapp.com/)
+
+## Project Contributors
+* Isabella Aguilera: Project Manager/Front-End/Back-End
+* Patrick Beljan: Back-End
+* Satasreea Dash: Back-End
+* Pamela Das: Back-End
+* Swetha Vikas: Back-End
+* Vanessa: Front-End
---
+++ prediction.py
@@ -0,0 +1,103 @@
+import twitter_data
+import stock_data
+from twitter_data import data_creator_predict
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import h5py
+import tensorflow as tf
+from tensorflow.python.keras.models import load_model
+import json
+
+def xScaler():
+    x = np.array([[1.16666667, 0.66666667],[0.6131379 , 1.52368464],[0.9011529 , 1.42572293],[0.91221833, 1.75215763],[0.35060131, 1.75027643],[1.33333333, 0.93333333],[0.0, 2.79100529],[0.3488813 , 1.69110366],[1.0224359 , 3.37703963]])
+    
+    X_scaler = StandardScaler().fit(x)
+    return X_scaler
+
+def ml_data_formatter(data5): 
+    try:
+        data5['Price Change_up'].astype('int64')
+    except:
+        try:
+            data5['Price Change_down'].astype('int64')
+            if data5.iloc[0,3] == 1:
+                data5.iloc[0,3] = 0
+            else:
+                data4.iloc[0,3] = 1
+        except:
+            try:
+                data5 = data5.T
+                data5['Price Change_up'].astype('int64')
+            except:
+                try:
+                    data5['Price Change_down'].astype('int64')
+                    if data5.iloc[0,3] == 1:
+                        data5.iloc[0,3] = 0
+                    else:
+                        data4.iloc[0,3] = 1
+                finally:
+                    print('Not a valid company')
+    data6x = []
+    tempx = np.array([])
+    for i in range(len(data5)):
+        negative = data5.iloc[i,0]
+        positive = data5.iloc[i,1]
+        if i == 0:
+            tempx = np.append(tempx, [negative, positive])
+        else:
+            tempx[0][0] += negative
+            tempx[0][1] += positive
+    data6x.append(tempx)
+        
+    #print(data6x)
+    X = np.asarray(data6x)
+    #print(X)
+    X_scaler = xScaler()
+    X_scaled = X_scaler.transform(X)
+    
+    return X_scaled
+
+def get_sentiment(df):
+    negative = []
+    positive = []
+    for i in range(len(df)):
+        negative.append(df.iloc[i,0])
+        positive.append(df.iloc[i,1])
+    neg_avg = sum(negative)/len(negative)
+    pos_avg = sum(positive)/len(positive)
+    averages = [neg_avg, pos_avg]
+    return averages
+
+def stock_prediction(X, model):
+    prediction = model.predict_classes(X)
+    if prediction == 1:
+        return 'Buy'
+    else:
+        return 'Sell'
+
+def get_Prediction(company_name):
+    model = load_model("Twitter_Sentiment_Stock_Model.h5")
+    try:
+        data = data_creator_predict(company_name)
+    except:
+        print('Not a valid company name')
+        return None
+    ml_data = ml_data_formatter(data)
+    prediction = stock_prediction(ml_data, model)
+    json_return = {}
+    json_return["prediction"] = prediction
+    averages = get_sentiment(data)
+    json_return["positive"] = averages[1]
+    json_return["negative"] = averages[0]
+    json_return["Company_Name"] = company_name
+    try:
+        json_return["Closing_Prices"] = stock_data.find_ticker_data(company_name).iloc[-5:,0].tolist()
+    except:
+        print('Not a valid company name')
+        return None
+    json_return2 = json.dumps(json_return)
+    return json_return2
+
+if __name__ == '__main__':
+    pass
